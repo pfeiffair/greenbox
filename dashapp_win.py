dd17ecee4dc008fb45d24ad04e72555c899cb827
JAlbertOfficial <jul_albert@proton.me>
2024-05-06 15:09:59 +0200

    feat: remove humidity and fan control, create 4 subpages (realtime view, historic view, config, misc), implement suppress_callback_exceptions to handle dynamic component generation in callbacks

--- dashapp_win.py
+++ dashapp_win.py
@@ -2,31 +2,9 @@
 # Packages
 ###############################################################
 
-from dash import Dash, dcc, html, Input, Output, State, callback
+from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as px
 import pandas as pd
-import sys
-import threading
-import time
-
-################################################################
-# Check for Raspberry Pi and GPIO Initialization
-###############################################################
-
-# Check if RPi module is available
-if 'RPi' in sys.modules:
-    # Import GPIO module for Raspberry Pi
-    import RPi.GPIO as GPIO
-    # Set GPIO mode
-    GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers
-    # Define GPIO pins for relay control
-    RELAIS_1_GPIO = 4
-    RELAIS_2_GPIO = 17
-    RELAIS_3_GPIO = 27
-    RELAIS_4_GPIO = 22
-else:
-    # If RPi module is not available, set GPIO to None
-    GPIO = None
 
 ################################################################
 # Dash App Initialization
@@ -37,118 +15,17 @@ app = Dash(__name__)
 
 # Define app layout
 app.layout = html.Div([
-    ################################################################
-    # Visualization Section
-    ###############################################################
-
-    html.Div(children=[
-        html.H4('Data Visualization: 2x BME280 Sensors'),
-
-        # Temperature Chart
-        dcc.Graph(id='time-series-chart-temp', style={'width': '100vh', 'height': '40vh','padding': 5}),
-        dcc.Interval(id='interval-component-t', interval=10*1000, n_intervals=0),
-
-        # Humidity Chart
-        dcc.Graph(id='time-series-chart-humid', style={'width': '100vh', 'height': '40vh','padding': 5}),
-        dcc.Interval(id='interval-component-humid', interval=10*1000, n_intervals=0),
-
-        # Vapor Pressure Deficit Chart
-        dcc.Graph(id='time-series-chart-vpd', style={'width': '100vh', 'height': '40vh','padding': 5}),
-        dcc.Interval(id='interval-component-vpd', interval=10*1000, n_intervals=0),
-    ], style={'width': '100vh', 'height': '100vh','padding': 5, 'flex': 1}), 
-
-    ################################################################
-    # Manual Control Section
-    ###############################################################
-
-    html.Div(children=[
-        html.H4('Control Fan and Humidifier Manually'),
-
-        # Fan Control
-        html.P("Fan control:"),
-        html.Div(id='container-button-basic', children='Enter a value in seconds and press Start'),
-        html.Div(dcc.Input(id='input-on-submit', type='text')),
-        html.Button('Start', id='submit-val', n_clicks=0),
-
-        # Humidifier Control
-        html.P("Humidifier control:"),
-        html.Div(id='container-button-humid', children='Enter a value in seconds and press Start'),
-        html.Div(dcc.Input(id='input-on-submit-humid', type='text')),
-        html.Button('Start', id='submit-val-humid', n_clicks=0),
-
-        # Camera Image
-        html.Img(src=app.get_asset_url('cam.png'), id='campic')
-    ], style={'padding': 10,'width': '50vh', 'flex': 1})
-], style={'display': 'flex', 'flexDirection': 'row'})
-
-################################################################
-# Helper Functions
-###############################################################
-
-# Function to run a job in a separate thread
-def run_threaded(job_func):
-    job_thread = threading.Thread(target=job_func)
-    job_thread.start()
-
-################################################################
-# Relay Control Functions
-###############################################################
-
-# Function to turn on Relay 1
-def R1on():
-    if GPIO:
-        GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
-        print("R1 on")
-        GPIO.output(RELAIS_1_GPIO, GPIO.HIGH) # on
-
-# Function to turn off Relay 1
-def R1off():
-    if GPIO:
-        GPIO.setup(RELAIS_1_GPIO, GPIO.OUT) # GPIO Assign mode
-        print("R1 off")
-        GPIO.output(RELAIS_1_GPIO, GPIO.LOW) # off
-
-# Function to turn on Relay 2
-def R2on():
-    if GPIO:
-        GPIO.setup(RELAIS_2_GPIO, GPIO.OUT) # GPIO Assign mode
-        print("R2 on")
-        GPIO.output(RELAIS_2_GPIO, GPIO.HIGH) # on
-
-# Function to turn off Relay 2
-def R2off():
-    if GPIO:
-        GPIO.setup(RELAIS_2_GPIO, GPIO.OUT) # GPIO Assign mode
-        print("R2 off")
-        GPIO.output(RELAIS_2_GPIO, GPIO.LOW) # off
-
-# Function to turn on Relay 3
-def R3on():
-    if GPIO:
-        GPIO.setup(RELAIS_3_GPIO, GPIO.OUT) # GPIO Assign mode
-        print("R3 on")
-        GPIO.output(RELAIS_3_GPIO, GPIO.HIGH) # on
-
-# Function to turn off Relay 3
-def R3off():
-    if GPIO:
-        GPIO.setup(RELAIS_3_GPIO, GPIO.OUT) # GPIO Assign mode
-        print("R3 off")
-        GPIO.output(RELAIS_3_GPIO, GPIO.LOW) # off
-
-# Function to turn on Relay 4
-def R4on():
-    if GPIO:
-        GPIO.setup(RELAIS_4_GPIO, GPIO.OUT) # GPIO Assign mode
-        print("R4 on")
-        GPIO.output(RELAIS_4_GPIO, GPIO.HIGH) # on
-
-# Function to turn off Relay 4
-def R4off():
-    if GPIO:
-        GPIO.setup(RELAIS_4_GPIO, GPIO.OUT) # GPIO Assign mode
-        print("R4 off")
-        GPIO.output(RELAIS_4_GPIO, GPIO.LOW) # off
+    # Navigation Tabs
+    dcc.Tabs(id='tabs', value='tab-realtime', children=[
+        dcc.Tab(label='Realtime View', value='tab-realtime'),
+        dcc.Tab(label='Historic View', value='tab-historic'),
+        dcc.Tab(label='Config', value='tab-config'),
+        dcc.Tab(label='Misc', value='tab-misc'),
+    ]),
+    
+    # Content of the tabs
+    html.Div(id='tabs-content')
+])
 
 ################################################################
 # Data Loading Function
@@ -164,112 +41,121 @@ def loaddata():
 # Callbacks
 ###############################################################
 
-# Fan Control Callback
-@callback(
-    Output('container-button-basic', 'children'),
-    Input('submit-val', 'n_clicks'),
-    State('input-on-submit', 'value'),
-    prevent_initial_call=True
-)
-def update_output(n_clicks, value):
-       
-    def windon(duration = 30):
-        print("Wind on for " + str(duration)+ " seconds")
-        R1on()
-        time.sleep(duration)
-        R1off()
-    
-    windon(duration=int(value))
+# Callback to update the content based on the selected tab
+@app.callback(Output('tabs-content', 'children'),
+              Input('tabs', 'value'))
+def render_content(tab):
+    if tab == 'tab-realtime':
+        return html.Div(children=[
+            ################################################################
+            # Visualization Section
+            ###############################################################
 
-    return 'Fan running for {} seconds and the button has been clicked {} times'.format(value, n_clicks)
+            html.Div(children=[
+                html.H4('Data Visualization: 2x BME280 Sensors'),
 
-# Humidifier Control Callback
-@callback(
-    Output('container-button-humid', 'children'),
-    Input('submit-val-humid', 'n_clicks'),
-    State('input-on-submit-humid', 'value'),
-    prevent_initial_call=True
-)
-def update_output(n_clicks, value):
-    
-  
-    def humidon(duration=30):
-        print("Humidifier on for "+ str(duration) +" seconds")
-        R2on()
-        time.sleep(2)
-        R3on()
-        time.sleep(1)
-        R3off()
-        time.sleep(duration)
-        R2off()
-        print("Humidifier off")
-    
-    run_threaded(humidon(duration=int(value)))
-    return 'Humidifier is running for {} seconds and the button has been clicked {} times'.format(value, n_clicks)
+                # Temperature Chart
+                dcc.Graph(id='time-series-chart-temp', style={'width': '100vh', 'height': '40vh','padding': 5}),
+                dcc.Interval(id='interval-component-t', interval=10*1000, n_intervals=0),
 
-# Temperature Chart Update Callback
+                # Humidity Chart
+                dcc.Graph(id='time-series-chart-humid', style={'width': '100vh', 'height': '40vh','padding': 5}),
+                dcc.Interval(id='interval-component-humid', interval=10*1000, n_intervals=0),
+
+                # Vapor Pressure Deficit Chart
+                dcc.Graph(id='time-series-chart-vpd', style={'width': '100vh', 'height': '40vh','padding': 5}),
+                dcc.Interval(id='interval-component-vpd', interval=10*1000, n_intervals=0),
+            ], style={'width': '100vh', 'height': '100vh','padding': 5, 'flex': 1})
+        ])
+    elif tab == 'tab-historic':
+        return html.Div(children=[
+            html.H4('Historic View')
+            # Add your content for Historic View here
+        ])
+    elif tab == 'tab-config':
+        return html.Div(children=[
+            html.H4('Config')
+            # Add your content for Config here
+        ])
+    elif tab == 'tab-misc':
+        return html.Div(children=[
+            html.H4('Misc')
+            # Add your content for Misc here
+        ])
+
+# Callback to update the Temperature Chart
 @app.callback(
     Output("time-series-chart-temp", "figure"), 
     Input('interval-component-t', 'n_intervals'))
-def display_time_series(ticker):
+def display_time_series_temp(ticker):
     df = loaddata()
     df = df[((df['type'] == 'Temp1') | (df['type'] == 'Temp2'))]
     
-    fig = px.line(df, x="date", y="value", color = "type", 
-                    labels=dict(date="Time", value="Temperature (°C)", type="Sensor"))
+    fig = px.line(df, x="date", y="value", color="type", 
+                  labels=dict(date="Time", value="Temperature (°C)", type="Sensor"))
     fig.update_xaxes(rangeselector=dict(
-                        buttons=list([
-                        dict(count=10, label="10 mim", step="minute", stepmode="backward"),
-                        dict(count=2, label="2 h", step="hour", stepmode="backward"),
-                        dict(count=12, label="12 h", step="hour", stepmode="todate"),
-                        dict(count=2, label="2 days", step="day", stepmode="backward"),
-                        dict(step="all") ])))
+        buttons=list([
+            dict(count=10, label="10 mim", step="minute", stepmode="backward"),
+            dict(count=2, label="2 h", step="hour", stepmode="backward"),
+            dict(count=12, label="12 h", step="hour", stepmode="todate"),
+            dict(count=2, label="2 days", step="day", stepmode="backward"),
+            dict(step="all") 
+        ])
+    ))
     fig.update_layout(title_text="Temperature in °C", title_font_size=30)
     fig.update_layout(uirevision="fix")
     return fig
 
-# Humidity Chart Update Callback
+# Callback to update the Humidity Chart
 @app.callback(
     Output("time-series-chart-humid", "figure"),
     Input('interval-component-humid', 'n_intervals'))
-def display_time_series(ticker):
+def display_time_series_humid(ticker):
     df = loaddata()
     df = df[((df['type'] == 'Humid1') | (df['type'] == 'Humid2'))]
     
-    fig = px.line(df, x="date", y="value", color = "type",labels=dict(date="Time", value="Humidity (%)", type="Sensor"))
+    fig = px.line(df, x="date", y="value", color="type",
+                  labels=dict(date="Time", value="Humidity (%)", type="Sensor"))
     fig.update_layout(title_text="Humidity in %", title_font_size=30, yaxis_range=[30,100])
     fig.update_yaxes(nticks=8)
     fig.update_xaxes(rangeslider_visible=True, 
                      rangeselector=dict(
-                        buttons=list([
-                        dict(count=10, label="10 mim", step="minute", stepmode="todate"),
-                        dict(count=2, label="2 h", step="hour", stepmode="todate"),
-                        dict(count=12, label="12 h", step="hour", stepmode="todate"),
-                        dict(count=2, label="2 days", step="day", stepmode="todate"),
-                        dict(step="all") ])))
+                         buttons=list([
+                             dict(count=10, label="10 mim", step="minute", stepmode="todate"),
+                             dict(count=2, label="2 h", step="hour", stepmode="todate"),
+                             dict(count=12, label="12 h", step="hour", stepmode="todate"),
+                             dict(count=2, label="2 days", step="day", stepmode="todate"),
+                             dict(step="all") 
+                         ])
+                     ))
     fig['layout']['uirevision'] = 'some-constant'
     return fig
 
-# Vapor Pressure Deficit Chart Update Callback
+# Callback to update the Vapor Pressure Deficit Chart
 @app.callback(
     Output("time-series-chart-vpd", "figure"),
     Input('interval-component-vpd', 'n_intervals'))
-def display_time_series(ticker):
+def display_time_series_vpd(ticker):
     df = loaddata()
     df = df[((df['type'] == 'vpd1') | (df['type'] == 'vpd2'))]
-    fig = px.line(df, x="date", y="value", color = "type",labels=dict(date="Time", value="VPD", type="Sensor"))
+    
+    fig = px.line(df, x="date", y="value", color="type",
+                  labels=dict(date="Time", value="VPD", type="Sensor"))
     fig.update_layout(title_text="Vapor Pressure Deficit", title_font_size=30)
     fig.update_yaxes(nticks=8)
     fig.update_xaxes(rangeslider_visible=True, 
                      rangeselector=dict(
-                        buttons=list([
-                        dict(count=10, label="10 mim", step="minute", stepmode="backward"),
-                        dict(count=2, label="2 h", step="hour", stepmode="backward"),
-                        dict(count=12, label="12 h", step="hour", stepmode="todate"),
-                        dict(count=2, label="2 days", step="day", stepmode="backward"),
-                        dict(step="all") ])))
+                         buttons=list([
+                             dict(count=10, label="10 mim", step="minute", stepmode="backward"),
+                             dict(count=2, label="2 h", step="hour", stepmode="backward"),
+                             dict(count=12, label="12 h", step="hour", stepmode="todate"),
+                             dict(count=2, label="2 days", step="day", stepmode="backward"),
+                             dict(step="all") 
+                         ])
+                     ))
     fig['layout']['uirevision'] = 'some-constant'
     return fig
 
 # Run the Dash app
+app.config.suppress_callback_exceptions = True
 app.run_server(debug=True)
